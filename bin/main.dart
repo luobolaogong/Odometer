76e94babcaff0becd22f4acd59efbb92ac49ae4f
Luobo Laogong <LuoboLaogong@GMail.com>
2017-09-17 21:21:03 -1000

    Just testing this push.

--- bin/main.dart
+++ bin/main.dart
@@ -1,5 +1,4 @@
-// Copyright (c) 2017, rob. All rights reserved. Use of this source code
-// is governed by a BSD-style license that can be found in the LICENSE file.
+
 import 'dart:math';
 
 import 'package:Odometer/Odometer.dart' as Odometer;
